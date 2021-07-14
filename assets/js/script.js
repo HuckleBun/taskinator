068ca1ab36cf937ae7654212b3720f8fd701acd5
Micah Duehring <micah@theduehrings.com>
2021-07-14 16:35:28 -0500

    add preventDefault function to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskFormEl = document.querySelector("#task-form")
 
-function createTaskHandler() {
+function createTaskHandler(event) {
+    event.preventDefault();
+    
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "A new task has been added.";
     tasksToDoEl.appendChild(taskItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+taskFormEl.addEventListener("submit", createTaskHandler);
