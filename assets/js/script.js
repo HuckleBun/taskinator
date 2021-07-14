3a82476581f801774923d6184fd0a0ca9b49dbb9
Micah Duehring <micah@theduehrings.com>
2021-07-14 16:51:56 -0500

    finished initial form submittion

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,16 @@ var taskFormEl = document.querySelector("#task-form")
 
 function createTaskHandler(event) {
     event.preventDefault();
-    
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "A new task has been added.";
-    tasksToDoEl.appendChild(taskItemEl);
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 taskFormEl.addEventListener("submit", createTaskHandler);
